85ad206bcbad91dff7ca0b7e4403bfbc78f717d9
Kaeggame <Kaeggameyt@gmail.com>
2020-06-08 21:57:00 +0200

    Add /announce + add right admin
    /announce add with admin right, so only admin can do /announce

--- cfx-server-data-master/resources/announce/client.lua
+++ cfx-server-data-master/resources/announce/client.lua
@@ -3,3 +3,8 @@ RegisterCommand("announce", function(source, args)
      -- we have to concatenate the table because the 'args' cb return a table (array)
      -- the 2nd parameter in 'table.concat' is just spacing each args out
 end)
+
+RegisterNetEvent("no-perms")
+AddEventHandler("no-perms", function()
+    TriggerEvent("chatMessage", "[Error]", {255,0,0}, "Sorry, you don't have any perms")
+end)
--- cfx-server-data-master/resources/announce/server.lua
+++ cfx-server-data-master/resources/announce/server.lua
@@ -1,5 +1,9 @@
 RegisterServerEvent('announce')
 AddEventHandler('announce', function(param)
-  print('^7[^1Announcement^7]^5:'.. param)
-  TriggerClientEvent('chatMessage', -1, '^7[^1Announcement^7]^2', {0,0,0} --[[ this table is just rgb ]], param)
+    if IsPlayerAceAllowed(source, "administrator") then
+      print('^7[^1Announcement^7]^5:'.. param)
+      TriggerClientEvent('chatMessage', -1, '^7[^1Announcement^7]^2', {255,0,0} --[[ this table is just rgb ]], param)
+    else
+        TriggerClientEvent("no-perms", source)
+    end 
 end)
