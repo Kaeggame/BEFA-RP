d12299a5f4537990c9f6a0c975e58daacbe33443
NATHITOUNE <nathitounet86@yahoo.be>
2020-06-08 18:41:52 +0200

    Revert "Update guns-c.lua"
    This reverts commit ff0e07a2c674e86a3f9098c84caa0ebea009f269.

--- cfx-server-data-master/resources/guns/guns-c.lua
+++ cfx-server-data-master/resources/guns/guns-c.lua
@@ -11,8 +11,8 @@ Citizen.CreateThread(function()
         Citizen.Wait(1)
          if IsControlJustReleased(1, h_key) then
             print("The Key".. h_key .. "was pressed")
-            giveWeapon("WEAPON_PISTOL")
-            giveWeapon("WEAPON_KNIFE")
+            giveWeapon("weapon_pistol")
+            giveWeapon("weapon_knife")
             alert("~b~Give Weapons with ~INPUT_VEH_HEADLIGHT~")
          end
     end
