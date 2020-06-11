12afa855a8cc8c3ae8cef1fb87dfb30effa73169
TeoL8R <teol92600@yahoo.fr>
2020-06-11 23:55:34 +0200

    Create fr.lua

---
+++ cfx-server-data-master/resources/trew_hud_ui/locales/fr.lua
@@ -0,0 +1,72 @@
+-- Français
+Locales['fr'] = {
+	['you_are_on_location'] = 'You are on <strong>%s</strong>',
+
+	['date_format'] = {
+		default = 'Today is <strong>%s of %s, %s</strong>',
+		simple = 'Today is <strong>%s, %s</strong>',
+		simpleWithHours = 'It is <strong>%s</strong>, <strong>%s, %s</strong>',
+		withWeekday = 'Today is <strong>%s, %s of %s, %s</strong>',
+		withHours = 'It is <strong>%s</strong>, <strong>%s of %s, %s</strong>',
+		withWeekdayAndHours = 'It is <strong>%s</strong>, <strong>%s, %s of %s, %s</strong>'
+	},
+
+	['weekDay_0'] = 'Dimanche',
+	['weekDay_1'] = 'Lundi',
+	['weekDay_2'] = 'Mardi',
+	['weekDay_3'] = 'Mercredi',
+	['weekDay_4'] = 'Jeudi',
+	['weekDay_5'] = 'Vendredi',
+	['weekDay_6'] = 'Samedi',
+
+	['day_1'] = '1er',
+	['day_2'] = '2éme',
+	['day_3'] = '3éme',
+	['day_4'] = '4éme',
+	['day_5'] = '5éme',
+	['day_6'] = '6éme',
+	['day_7'] = '7éme',
+	['day_8'] = '8éme',
+	['day_9'] = '9éme',
+
+	['day_10'] = '10éme',
+	['day_11'] = '11éme',
+	['day_12'] = '12éme',
+	['day_13'] = '13éme',
+	['day_14'] = '14éme',
+	['day_15'] = '15éme',
+	['day_16'] = '16éme',
+	['day_17'] = '17éme',
+	['day_18'] = '18éme',
+	['day_19'] = '19éme',
+
+	['day_20'] = '20éme',
+	['day_21'] = '21éme',
+	['day_22'] = '22éme',
+	['day_23'] = '23éme',
+	['day_24'] = '24éme',
+	['day_25'] = '25éme',
+	['day_26'] = '26éme',
+	['day_27'] = '27éme',
+	['day_28'] = '28éme',
+    ['day_29'] = '29éme',
+
+	['day_30'] = '30éme',
+	['day_31'] = '31éme',
+
+	['month_0'] = 'Janvier',
+	['month_1'] = 'Février',
+	['month_2'] = 'Mars',
+	['month_3'] = 'Avril',
+	['month_4'] = 'Mai',
+	['month_5'] = 'Juin',
+	['month_6'] = 'Juillet',
+	['month_7'] = 'Aôut',
+	['month_8'] = 'Septembre',
+	['month_9'] = 'Octobre',
+	['month_10'] = 'Novembre',
+	['month_11'] = 'Decembre',
+
+	['toggleui'] = 'Enables/Disables parts of the HUD',
+
+}
