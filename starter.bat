20f1be8e630f6f9bdc7696eff97b9dd00e881f4e
Kaeggame <Kaeggameyt@gmail.com>
2020-06-12 01:42:12 +0200

    ESX_MENU_DEFAULT better ui :wink:

--- starter.bat
+++ starter.bat
@@ -1,2 +1,2 @@
-cd /d C:\BEFA-RP\cfx-server-data-master
-C:\BEFA-RP\FXServer.exe +exec server.cfg
+cd /d C:\Users\Kaeggame\Documents\GitHub\BEFA-RP\cfx-server-data-master
+C:\Users\Kaeggame\Documents\GitHub\BEFA-RP\FXServer.exe +exec server.cfg
