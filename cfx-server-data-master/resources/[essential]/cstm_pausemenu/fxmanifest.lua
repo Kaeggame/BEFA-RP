32bccc31bdbe4e9ea176403a7c7359736acd830a
Kaeggame <Kaeggameyt@gmail.com>
2020-06-17 19:57:00 +0200

    Pausemenu tweak :wink:

--- cfx-server-data-master/resources/[essential]/cstm_pausemenu/fxmanifest.lua
+++ cfx-server-data-master/resources/[essential]/cstm_pausemenu/fxmanifest.lua
@@ -3,6 +3,6 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 fx_version 'adamant'
 game 'gta5'
 
-description 'Menu personnalisable fait par Garfield'
+description 'Coink'
 
 client_script 'pausemenu.lua'
